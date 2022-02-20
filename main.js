5b3b0812c399b21bb01e7db44886e665d3d2c6fb
NavyaPoddar <84183648+NavyaPoddar@users.noreply.github.com>
2022-02-20 15:42:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function setup{
+function setup(){
     canvas = createCanvas(500,500);
-    canvas.center();
+    canvas.position(700,200);
+    video = createCapture(VIDEO);
+    video.size(600,500);
+    video.position(100,200);
+    posenetmodel = ml5.poseNet(video ,modelloaded);
+    posenetmodel.on("pose" , getresults);
+    
+}
+
+function modelloaded(){
+    console.log("model has loaded")
+}
+
+function getresults(resultsaaray){
+    if(resultsaaray.lenght > 0 ){
+        console.log(resultsaaray);
+    }
 }
